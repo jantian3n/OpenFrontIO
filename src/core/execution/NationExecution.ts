552f67e0d51cb254b728fdf6bd9516da1e7e5ce5
filfakmsuru <yantya@my.msu.ru>
2026-09-23 10:56:16 +0800

    feat(subject): process protection obligations in nation AI

--- src/core/execution/NationExecution.ts
+++ src/core/execution/NationExecution.ts
@@ -198,6 +198,7 @@ export class NationExecution implements Execution {
 
     this.emojiBehavior.maybeSendCasualEmoji();
     this.updateRelationsFromEmbargos();
+    this.allianceBehavior.handleProtectionCalls();
     this.allianceBehavior.handleSubjectRequests();
     this.allianceBehavior.handleAllianceRequests();
     this.allianceBehavior.handleAllianceExtensionRequests();
